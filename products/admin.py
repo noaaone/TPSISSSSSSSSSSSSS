0eca6a79708b3de9c728f07947d2f19386d35595
Vlad <vladnewgod@gmail.com>
2024-04-27 17:10:56 +0300

    edit

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'manufacturer', 'product_code', 'supplier_contact', 'supplier_country', 'count')
+    search_fields = ['product_name', 'product_code', 'manufacturer', 'supplier_country']
+    list_filter = ['manufacturer', 'supplier_country']
--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=255, verbose_name='Название препарата')
+    manufacturer = models.CharField(max_length=255, verbose_name='Производитель')
+    product_code = models.CharField(max_length=100, verbose_name='Код товара', unique=True)
+    supplier_contact = models.CharField(max_length=255, verbose_name='Данные поставщика')
+    supplier_country = models.CharField(max_length=100, verbose_name='Страна-поставщик')
+    count = models.IntegerField(verbose_name='Количество товаров')  # Заменяем поле status на count
+
+    def __str__(self):
+        return f'{self.product_name} ({self.product_code})'
